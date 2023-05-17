ac91f2815b18cde3c9405f0dcfad9338b84a29bd
Margus Liinev <margus.liinev@gmail.com>
2023-05-17 20:05:55 +0300

    production error fix attempt 4

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import Wrapper from './assets/wrappers/home.js';
 
 function App() {
     return (
@@ -15,28 +15,5 @@ function App() {
         </Wrapper>
     );
 }
-
-const Wrapper = styled.div`
-    min-width: 100vw;
-    min-height: 100vh;
-    background-color: #333;
-    color: var(--colorWhite);
-    display: grid;
-    .container {
-        width: 90vw;
-        margin: 0 auto;
-        max-width: var(--width-lg);
-    }
-    h1 {
-        margin: 5rem 0 2rem 0;
-    }
-    p {
-        font-size: 1.25rem;
-        margin-bottom: 1rem;
-    }
-    button {
-        font-size: 1.25rem;
-    }
-`;
 
 export default App;
