d7893d7b1ffe0ff5897337eab4bc4b88896fe15d
Margus Liinev <margus.liinev@gmail.com>
2023-05-17 19:52:09 +0300

    production fix attempt 3

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
-import macrosPlugin from 'vite-plugin-babel-macros';
-import commonjs from 'vite-plugin-commonjs';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), macrosPlugin(), commonjs()],
+    plugins: [react()],
 });
