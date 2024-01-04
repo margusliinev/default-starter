2b5fb3a34cd0d1ce25fc7dd9d60883032b3a0c0e
Margus Liinev <margus.liinev@gmail.com>
2024-01-04 22:40:07 +0200

    optimized background image, added shadcn form example

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,32 @@
-import { useEffect, useState } from 'react';
 import { ThemeToggle } from './components/theme-toggle';
+import { Button } from './components/ui/button';
+import { Input } from './components/ui/input';
+import { Label } from './components/ui/label';
 
 function App() {
-    const [data, setData] = useState('');
-
-    useEffect(() => {
-        void fetch('/api/v1/healthcheck')
-            .then((res) => {
-                if (res.status === 200) {
-                    return 'Healthcheck successful!';
-                } else {
-                    return 'Healthcheck failed!';
-                }
-            })
-            .then((data) => setData(data));
-    }, []);
-
     return (
         <main className='w-screen h-screen grid place-items-center'>
             <span className='absolute top-8 right-8'>
                 <ThemeToggle />
             </span>
-            <h1 className='text-xl font-medium'>{data}</h1>
+            <form className='grid gap-4 w-screen-90 max-w-sm p-6 bg-background shadow-md border rounded-md ' autoComplete='off'>
+                <h1 className='text-lg font-semibold text-center'>Create an account</h1>
+                <fieldset className='space-y-1'>
+                    <Label htmlFor='username'>Username</Label>
+                    <Input id='username' name='username' type='text'></Input>
+                </fieldset>
+                <fieldset className='space-y-1'>
+                    <Label htmlFor='email'>Email</Label>
+                    <Input id='email' name='email' type='email'></Input>
+                </fieldset>
+                <fieldset className='space-y-1'>
+                    <Label htmlFor='password'>Password</Label>
+                    <Input id='password' name='password' type='password'></Input>
+                </fieldset>
+                <Button type='submit' size={'sm'} className='mt-2'>
+                    Register
+                </Button>
+            </form>
         </main>
     );
 }
