efcfc00352dd9dbfd9968fd9af789779ea7b97d7
Margus Liinev <margus.liinev@gmail.com>
2024-01-05 17:30:05 +0200

    added check for successful api connection and added sonner notifications

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,42 @@
+import { useEffect } from 'react';
 import { ThemeToggle } from './components/theme-toggle';
 import { Button } from './components/ui/button';
 import { Input } from './components/ui/input';
 import { Label } from './components/ui/label';
+import { toast } from 'sonner';
+
+type HealthcheckAPIResponse = {
+    success: boolean;
+    data: null;
+};
 
 function App() {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        toast.success('Your account has been created!');
+    };
+
+    useEffect(() => {
+        void fetch('/api/v1/healthcheck')
+            .then((res) => res.json())
+            .then((data: HealthcheckAPIResponse) => {
+                if (data.success) {
+                    toast.success('Connencted to the server!');
+                }
+            });
+    }, []);
+
     return (
         <main className='w-screen h-screen grid place-items-center'>
-            <span className='absolute top-8 right-8'>
-                <ThemeToggle />
-            </span>
-            <form className='grid gap-4 w-screen-90 max-w-sm p-6 bg-background shadow-md border rounded-md ' autoComplete='off'>
+            <nav className='fixed top-0 h-16 w-full grid place-items-center'>
+                <div className='flex items-center justify-between w-screen-90'>
+                    <h1 className='text-2xl font-semibold'>Default Starter</h1>
+                    <div className='w-48 grid justify-items-end'>
+                        <ThemeToggle />
+                    </div>
+                </div>
+            </nav>
+            <form className='grid gap-4 w-screen-90 max-w-sm p-6 bg-background shadow-md border rounded-md' noValidate autoComplete='off' onSubmit={handleSubmit}>
                 <h1 className='text-lg font-semibold text-center'>Create an account</h1>
                 <fieldset className='space-y-1'>
                     <Label htmlFor='username'>Username</Label>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from '@/components/theme-provider';
-import React from 'react';
+import { Toaster } from '@/components/ui/sonner';
 import ReactDOM from 'react-dom/client';
+import React from 'react';
 import App from './App.tsx';
 import './styles/fonts.css';
 import './styles/index.css';
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
             <App />
+            <Toaster />
         </ThemeProvider>
     </React.StrictMode>,
 );
