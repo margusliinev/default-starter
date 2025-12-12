11e9521bab666325353c63214596fd56766a74f2
Margus Liinev <margus.liinev@gmail.com>
2025-12-13 00:32:23 +0200

    share types

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -1,10 +1,16 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static';
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
-    kit: { adapter: adapter() },
+    kit: {
+        adapter: adapter(),
+        alias: {
+            $shared: path.resolve('../shared'),
+        },
+    },
 };
 
 export default config;
